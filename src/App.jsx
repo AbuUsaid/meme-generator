85db596edf7fbb2a52e2dc021dd39befeb664f3f
AbuUsaid <usaidabu321@gmail.com>
2023-11-18 20:12:17 +0530

    Make a header component and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import './style.css';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
