85af929928c51a96acb08ca75dfa6456aac6cb54
AbuUsaid <usaidabu321@gmail.com>
2023-11-22 08:04:58 +0530

    Add images to the meme generator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="header">
       <img src="src\images\troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">Create Memes Easily</h4>
     </header>
   );
 };
